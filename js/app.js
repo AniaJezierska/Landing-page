187f9128c681bf993b42a28ed1a21963a8e380cc
AniaJezierska <candyrockstore5@gmail.com>
2020-12-23 11:28:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@
  * Define Global Variables
  * 
 */
- const navBar = document.getElementById("navbar__list");
- const sections = document.querySelectorAll("section");
+const navBar = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
 
 
 /**
